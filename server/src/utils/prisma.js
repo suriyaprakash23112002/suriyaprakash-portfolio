9156eacc7240cf4d7b0dafc7e08501c0bfb35f5c
suriyaprakash23112002 <suriyaprakashkumaran567@gmail.com>
2026-09-20 08:39:00 +0530

    Import Prisma client from package for Vercel

--- server/src/utils/prisma.js
+++ server/src/utils/prisma.js
@@ -1,12 +1,11 @@
 import "dotenv/config";
 
 import { PrismaPg } from "@prisma/adapter-pg";
-
-import { PrismaClient } from "../generated/prisma/client.ts";
+import { PrismaClient } from "@prisma/client";
 
 if (!process.env.DATABASE_URL) {
   throw new Error(
-    "DATABASE_URL is missing from the .env file."
+    "DATABASE_URL is missing from the environment."
   );
 }
 
